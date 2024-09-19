48f958eb2574140f32fc5b1e99f07c2c3d723fd6
dipen-gohel <dipengohel123456@gmail.com>
2024-09-19 12:33:03 -0400

    Update CardTrick.java
    new

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -61,6 +61,7 @@ public class CardTrick {
         
         
     }
+    /* dipen */
     public static int randomNumberGenerator(int min, int max) {
         Random random = new Random();
         return random.nextInt(max - min + 1) + min;
