aa6ed25b3929f5da0d2356539ff6ff4395e00ffc
dipen-gohel <dipengohel123456@gmail.com>
2024-09-19 12:27:54 -0400

    Update CardTrick.java
    Hard code lucky card

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -16,13 +16,6 @@ package card;
 
 import java.util.Random;
 import java.util.Scanner;
-
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
- */
 public class CardTrick {
     
     public static void main(String[] args)
@@ -37,10 +30,13 @@ public class CardTrick {
             magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit(Card.SUITS[2]); ]
+        
+        
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a card value (1-13): ");
         int cardValue = sc.nextInt();
