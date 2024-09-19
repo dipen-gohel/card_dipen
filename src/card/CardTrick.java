de726bb308b8109c4ddaca072dc7aedf96134651
dipen-gohel <dipengohel23456@gmail.com>
2024-09-19 11:58:35 -0400

    dipen version 1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -2,7 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+/**
+ * CardTrick class
+ * Modifier: Dipen Gohel
+ * Date: 19 9 2024
+ */
+
+
+
+
 package card;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -19,14 +32,42 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(randomNumberGenerator(1,13));
+            c.setSuit(Card.SUITS[randomNumberGenerator(0,3)]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a card value (1-13): ");
+        int cardValue = sc.nextInt();
+        System.out.println("Enter a suit (0-3 where 0=Hearts, 1=Diamonds, 2=Clubs, 3=Spades): ");
+        int cardSuit = sc.nextInt();
+        Card userCard = new Card();
+        userCard.setValue(cardValue);
+        userCard.setSuit(Card.SUITS[cardSuit]);
+        
+        boolean cardFound = false;
+        for(Card card : magicHand) {
+            if(card.getValue() == userCard.getValue() && card.getSuit().equals(userCard.getSuit())) {
+                cardFound = true;
+                break;
+            }
+        }
+        if (cardFound) {
+            System.out.println("Your card is in the magic hand!");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+        
+        
+    }
+    public static int randomNumberGenerator(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
     }
     
 }
